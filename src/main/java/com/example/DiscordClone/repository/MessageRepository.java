c526de86ba70d62a3a41239d563ec5076b37ec21
anilkr09 <anilmnnit20@gmail.com>
2025-03-02 13:05:42 +0530

    first commit

--- src/main/java/com/example/DiscordClone/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DiscordClone.repository;
-
-import com.example.DiscordClone.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
