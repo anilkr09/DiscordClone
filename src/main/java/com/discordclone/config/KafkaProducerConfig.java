cf2f40e1d60bf519022301a5c860ca68e38b705c
anil <anilmnnit20@gmail.com>
2026-04-02 21:43:59 +0530

    added Kafka broker configuration

---
+++ src/main/java/com/discordclone/config/KafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package com.discordclone.config;
+
+import com.discordclone.payload.MessageResponse;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Profile("kafka")
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, MessageResponse> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.RETRIES_CONFIG, 3);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, MessageResponse> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
