e14e9d2fecda74880a6d6c61644846ee12f1f62a
anil <anilmnnit20@gmail.com>
2026-04-07 18:14:58 +0530

    moved message persistance inside MessageEventPublisher

--- src/main/java/com/discordclone/service/MessageEventPublisher.java
+++ src/main/java/com/discordclone/service/MessageEventPublisher.java
@@ -1,7 +1,10 @@
 package com.discordclone.service;
 
+import com.discordclone.model.Message;
+import com.discordclone.model.User;
+import com.discordclone.payload.MessageRequest;
 import com.discordclone.payload.MessageResponse;
 
 public interface MessageEventPublisher {
-    void publish(MessageResponse message);
+    void publish(Message message, MessageResponse response, MessageRequest request, User user);
 }
